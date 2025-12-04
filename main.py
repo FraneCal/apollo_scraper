87dcbbfe082cf4e6352cfc789549d026f98fc0ba
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2025-12-04 08:08:28 +0100

    updated the code but not finished
    have to add cloudfare captcha solving for login, update data extraction, split the code into modules

--- main.py
+++ main.py
@@ -1,188 +1,200 @@
-import os
-import random
-import time
-import pandas as pd
-from itertools import zip_longest
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from twocaptcha import TwoCaptcha
+import time
+import os
 
-EMAIL = 'APOLLO EMAIL'
-PASSWORD = 'APOLLO PASSWORD'
-URL = "APOLLO SAVED LIST LINK"
+EMAIL = 'YOUR APOLLO EMAIL'
+PASSWORD = 'YOUR APOLLO PASSWORD'
+URL = "YOUR APOLLO SAVED LINK LIST"
+LOGIN_URL = "https://app.apollo.io/#/login?locale=en"
+TWO_CAPTCHA_API_KEY = "YOUR 2CAPTCHA API KEY"
 
-class ApolloScraper:
-    def __init__(self, user_agents, url):
-        '''Initialize the scraper with user agents, login credentials, and URL.'''
-        self.user_agents = user_agents
-        self.url = url
-        self.driver = self.setup_webdriver()
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0; IN) Gecko/20100101 Firefox/117.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 IN",
+    "Mozilla/5.0 (Linux; Android 11; RMX2185) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 10; SM-M315F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 12; M2101K7AI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/124.0.0.0 IN",
+    "Mozilla/5.0 (Linux; Android 13; CPH2381) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 13; SM-A536E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 12; 2201116TI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+]
 
-    def setup_webdriver(self):
-        '''Set up the Chrome WebDriver with random user-agent headers.'''
-        service = Service()
-        options = Options()
-        options.add_argument(f"user-agent={random.choice(self.user_agents)}")
-        # options.add_argument("--headless")
-        self.driver = webdriver.Chrome(service=service, options=options)
-        self.driver.maximize_window()
-        return self.driver
+CHROME_DRIVER_ARGUMENTS = [
+    '--disable-blink-features=AutomationControlled',
+    '--disable-infobars',
+    '--start-maximized',
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-gpu',
+    '--disable-extensions',
+    '--disable-application-cache',
+    '--disable-dev-shm-usage',
+    '--disable-popup-blocking',
+    '--check-for-update-interval=31536000',
+    '--disable-component-update',
+    '--no-default-browser-check',
+    "--simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'"
+]
 
-    def login(self):
-        '''Perform login to the Apollo website using email and password.'''
-        self.driver.get(self.url)
-        email_input = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'email')))
-        email_input.send_keys(EMAIL)
+STEALTH_JS = '''
+(() => {
+    delete Object.getPrototypeOf(navigator).webdriver;
+    Object.defineProperty(navigator, 'plugins', {
+        get: () => [
+            {0: {type: 'application/x-google-chrome-pdf', description: 'Portable Document Format'}},
+            {0: {type: 'application/pdf', description: 'Portable Document Format'}}
+        ]
+    });
+    Object.defineProperty(navigator, 'platform', {
+        get: () => (/(Android|Linux)/.test(navigator.userAgent) ? 'Linux armv8l' : 'Win32')
+    });
+    Object.defineProperty(navigator, 'languages', {
+        get: () => ['en-IN','en','hi-IN']
+    });
+    Object.defineProperty(navigator, 'connection', {
+        get: () => ({
+            downlink: 10,
+            effectiveType: '4g',
+            rtt: 100,
+            saveData: false,
+            type: 'wifi'
+        })
+    });
+    Object.defineProperty(navigator, 'hardwareConcurrency', {
+        get: () => 8
+    });
+    Object.defineProperty(navigator, 'deviceMemory', {
+        get: () => 8
+    });
+    window.chrome = {
+        app: { isInstalled: false },
+        webstore: { onInstallStageChanged: {}, onDownloadProgress: {} },
+        runtime: { }
+    };
+    Object.defineProperty(navigator, 'permissions', {
+        get: () => ({
+            query: (parameters) => (
+                parameters.name === 'notifications'
+                ? Promise.resolve({ state: Notification.permission })
+                : Promise.resolve({ state: 'granted' })
+            )
+        })
+    });
+})();
+'''
 
-        password_input = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
-        password_input.send_keys(PASSWORD)
+def captcha_solving():
+    api_key = os.getenv('APIKEY_2CAPTCHA', TWO_CAPTCHA_API_KEY)
 
-        log_in_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="provider-mounter"]/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div/form/div[4]/button')))
-        log_in_button.click()
+    solver = TwoCaptcha(api_key)
 
-        time.sleep(10)
+    try:
+        result = solver.turnstile(
+            sitekey='0x4AAAAAAA2OUu4yFDL9LSeZ',
+            url=LOGIN_URL,
+        )
 
-    def get_text_or_default(self, element, default=''):
-        '''Helper function to extract text from an element or return default if None.'''
-        return element.text.strip() if element else default
+    except Exception as e:
+        print(e)
 
-    def scrape_data(self, num_pages_to_scrape, excel_file_path):
-        '''Scrape data from the Apollo website and save it to an Excel file.'''
-        page_num = 0
+    else:
+        print('solved: ' + str(result))
 
-        print("Starting to scrape! :)")
+def login(email, password, url):
+    options = Options()
+    options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36')
 
-        while page_num < num_pages_to_scrape:
-            page_num += 1
-            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+    for arg in CHROME_DRIVER_ARGUMENTS:
+        options.add_argument(arg)
 
-            # Initialize the data dictionary for this page
-            page_data = {
-                'Business Name': [],
-                'Website': [],
-                'Niche': [],
-                'Country': [],
-                'First Name': [],
-                'Last Name': [],
-                'Job Title': [],
-                'Phone number': [],
-                'Personal email': [],
-                'Personal LinkedIn': [],
-                'Company LinkedIn': [],
-            }
+    driver = webdriver.Chrome(options=options)
 
-            # Extracting names
-            names = [name.text.split(maxsplit=1) for name in soup.find_all('div', class_='zp_xVJ20') if name.find('a')]
-            first_name, last_name = zip_longest(*names, fillvalue='')
+    driver.execute_cdp_cmd(
+        "Page.addScriptToEvaluateOnNewDocument",
+        {"source": STEALTH_JS}
+    )
 
-            # Extracting websites
-            websites = [a.get('href') for a in soup.find_all('a', class_='zp-link zp_OotKe')
-                        if a.get('href') and not a.get('href').startswith('#') and not any(social in a.get('href') for social in ["facebook", "linkedin", "twitter", "apollo", "Facebook"])]
-            company_names = [self.get_text_or_default(name.find('a'), "Company not found") for name in soup.find_all('div', class_='zp_J1j17')]
-            websites.extend(["Website not specified"] * (len(company_names) - len(websites)))
-            
-            # Extracting company LinkedIn links
-            company_linkedin_list = [a.get('href') for a in soup.find_all('a', class_='zp-link zp_OotKe')
-                                     if a.get('href') and "linkedin" in a.get('href') and "company" in a.get('href')]
-            company_linkedin_list.extend(["LinkedIn page not specified"] * (len(company_names) - len(company_linkedin_list)))
-            
-            # Extracting personal LinkedIn links
-            personal_linkedin_list = [a.get('href') for a in soup.find_all('a', class_='zp-link zp_OotKe')
-                                      if a.get('href') and "linkedin" in a.get('href') and "company" not in a.get('href')]
+    # Open the Login page
+    driver.get(url)
 
-            # Extracting industries
-            industries = soup.find_all('span', class_='zp_PHqgZ zp_TNdhR')[:25]
-            industries_list = [industry.text.strip() for industry in industries if industry is not None]
+    # Email field
+    try:
+        email_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'email')))
+        ActionChains(driver).click(email_input_field).send_keys(email).perform()
+    except TimeoutException:
+        print("Email field was not clicked.")
 
-            # Extracting locations
-            locations = soup.find_all('span', class_='zp_Y6y8d')
-            locations_list = [location.text.strip() for location in locations if location is not None and (location.text.strip().endswith(", Australia") or location.text.strip() == "Australia")]
+    # Password input field
+    try:
+        password_wrapper = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']"))).find_element(By.XPATH,'//*[@id="provider-mounter"]/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div[3]/form/div/div[2]/div[2]/div[1]')
+        ActionChains(driver).send_keys_to_element(password_wrapper, password).perform()
+    except TimeoutException:
+        print("Password field was not clicked.")
 
-            # Extracting job titles
-            titles = [self.get_text_or_default(title) for title in soup.find_all('span', class_='zp_Y6y8d')[::3]]
+    # time.sleep(1)
 
-            # Extracting company names
-            company_names = [self.get_text_or_default(name.find('a'), "Company not found") for name in soup.find_all('div', class_='zp_J1j17')]
+    # Log in button
+    try:
+        log_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="provider-mounter"]/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div[3]/form/div/button')))
+        ActionChains(driver).click(log_in_button).perform()
+    except TimeoutException:
+        print("No button found.")
 
-            # Extracting phone numbers
-            phone_numbers = soup.find_all('span', class_='zp_lm1kV')
-            phone_numbers_clean = [phone.find('a') for phone in phone_numbers]
-            phone_numbers = [phone.text.strip() for phone in phone_numbers_clean if phone is not None]
+    try:
+        captcha_solving()
+    except Exception as e:
+        print(f"captcha_solving() failed or not needed: {e}")
 
-            # Extracting emails
-            emails = [self.get_text_or_default(email.find('a', class_='zp-link zp_OotKe zp_Iu6Pf')) for email in soup.find_all('div', class_='zp_jcL6a')]
+    return driver
 
-            # Storing the scraped data in the dictionary
-            page_data['Business Name'].extend(company_names)
-            page_data['Website'].extend(websites)
-            page_data['Niche'].extend(industries_list)
-            page_data['Country'].extend(locations_list)
-            page_data['First Name'].extend(first_name)
-            page_data['Last Name'].extend(last_name)
-            page_data['Job Title'].extend(titles)
-            page_data['Phone number'].extend(phone_numbers)
-            page_data['Personal email'].extend(emails)
-            page_data['Personal LinkedIn'].extend(personal_linkedin_list)
-            page_data['Company LinkedIn'].extend(company_linkedin_list)
+def site_navigation(driver, url):
+    # Wait until the homepage appears
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'zp_gCXMt')))
 
-            # Save the data for this page
-            df = pd.DataFrame(page_data)
-            self.save_to_excel(df, excel_file_path)
+    # Go to the list link
+    driver.get(url)
 
-            # Proceed to next page if available
-            if page_num < num_pages_to_scrape:
-                try:
-                    print(f'Scraping page {page_num}/{num_pages_to_scrape}.')
-                    next_page_button = self.driver.find_element(By.XPATH, '//*[@id="main-app"]/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div/div/div/div/div[2]/div/div[4]/div/div/div/div/div[3]/div/div[2]/button[2]')
-                    next_page_button.click()
-                    time.sleep(3)
-                except NoSuchElementException:
-                    print("No next page button found.")
-                    break
+    # Wait until the list shows
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-testid='contact-name-cell']")))
 
-    def save_to_excel(self, df, excel_file_path):
-        '''Save the scraped data to an Excel file. If file exists, it appends new data.'''
-        if os.path.isfile(excel_file_path):
-            existing_df = pd.read_excel(excel_file_path)
-            df_no_duplicates = pd.concat([existing_df, df]).drop_duplicates(keep='last')
-            df_no_duplicates.to_excel(excel_file_path, index=False)
-        else:
-            df.to_excel(excel_file_path, index=False)
-        print(f"Data saved to {excel_file_path}")
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    def remove_duplicates(self, input_file, output_file):
-        '''Remove duplicate entries in the Excel file and save the cleaned data.'''
-        df = pd.read_excel(input_file)
-        if df.duplicated().any():
-            df_no_duplicates = df.drop_duplicates()
-            df_no_duplicates.to_excel(output_file, index=False)
-            print(f"Duplicate rows removed and saved to {output_file}")
-        else:
-            df.to_excel(output_file, index=False)
-            print("No duplicate rows found. Data remains unchanged.")
+    try:
+        next_page = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-container-column-2"]/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/button[2]')))
+        next_page.click()
+    except TimeoutException:
+        print("Email field was not clicked.")
 
-    def quit(self):
-        '''Close the WebDriver session.'''
-        self.driver.quit()
+    time.sleep(5)
 
+    page_source = driver.page_source
 
-if __name__ == "__main__":
-    user_agents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"]
+    driver.quit()
 
-    scraper = ApolloScraper(user_agents, URL)
+    soup = BeautifulSoup(page_source, "html.parser")
 
-    scraper.login()
+    names = [name.getText() if name is not None else "NA" for name in (soup.find_all('span', class_='zp_pHNm5'))]
+    companies = [company.getText() if company is not None else "NA" for i, company in enumerate(soup.find_all('span', class_='zp_xvo3G')) if i % 2 == 1]
+    emails = [email.getText() if email is not None else "NA" for i, email in enumerate(soup.find_all('span', class_='zp_xvo3G')) if i % 2 == 0]
+    job_titles = [job_title.getText() for i, job_title in enumerate(soup.find_all('div', class_='zp_YGDgt')) if i % 2 == 0]
+    locations = [location.getText() for i, location in enumerate(soup.find_all('div', class_='zp_YGDgt')) if i % 2 == 1]
+    social_media_links = [social_media_link.get("href") if social_media_link is not None else "NA" for social_media_link in (soup.find_all('div', class_='zp_qR5tW').find_all('a'))]
 
-    num_pages_to_scrape = 2
-    excel_file_path = 'data.xlsx'
-    scraper.scrape_data(num_pages_to_scrape, excel_file_path)
+    for s in social_media_links:
+        print(s)
 
-    scraper.quit()
+    print(len(social_media_links))
 
-    output_file_path = "cleaned_data.xlsx"
-    scraper.remove_duplicates(excel_file_path, output_file_path)
+driver = login(EMAIL, PASSWORD, LOGIN_URL)
+site_navigation(driver, URL)
